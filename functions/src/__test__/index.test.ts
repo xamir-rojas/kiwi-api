cbc65278463f5193fbc3b492c5f60d51b802dcf4
Xamir Rojas <xamir.e.rojas@gmail.com>
2022-03-20 12:29:10 -0500

    Tests of GET, GET id and POST Deliveries

--- functions/src/__test__/index.test.ts
+++ functions/src/__test__/index.test.ts
@@ -6,7 +6,8 @@ import admin from "firebase-admin";
 */
 
 // const myFunctions = require("../index");
-import {kiwiFunctions} from "../index";
+import { kiwiFunctions } from "../index";
+import { faker } from "@faker-js/faker";
 
 /*
 let adminInitStub;
@@ -22,6 +23,31 @@ afterAll(() => {
 });
 */
 
+const mockUpDelivery = {
+  creation_date: faker.date.recent(),
+  state: "pending",
+  pickup: {
+    pickup_lat: faker.address.latitude(),
+    pickup_lon: faker.address.longitude(),
+  },
+  dropoff: {
+    dropoff_lat: faker.address.latitude(),
+    dropoff_lon: faker.address.longitude(),
+  },
+  zone_id: faker.datatype.uuid(),
+};
+
+/*
+const mockUpBot = {
+  status: "available",
+  location: {
+    dropoff_lat: faker.address.latitude(),
+    dropoff_lon: faker.address.longitude(),
+  },
+  zone_id: faker.datatype.uuid(),
+};
+*/
+
 describe("Test example", () => {
   it("GET /", (done) => {
     request(kiwiFunctions)
@@ -30,6 +56,62 @@ describe("Test example", () => {
       .expect(200)
       .expect((res) => {
         expect(res.body.message).toEqual("Hello World!");
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+});
+
+describe("Deliveries API", () => {
+  it("GET All Deliveries ", (done) => {
+    request(kiwiFunctions)
+      .get("/api/v1/deliveries")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.data.length).toEqual(10);
+      })
+      .end((err, res) => {
+        if (err) return done(err);
+        return done();
+      });
+  });
+  it("GET /deliveries/:id", (done) => {
+    let deliveryId: string;
+    request(kiwiFunctions)
+      .post("/api/v1/deliveries")
+      .expect("Content-Type", /json/)
+      .send(mockUpDelivery)
+      .expect(201)
+      .expect((res) => {
+        deliveryId = res.body.data.id;
+      })
+      .end(() => {
+        request(kiwiFunctions)
+          .get("/api/v1/deliveries/" + deliveryId)
+          .expect("Content-Type", /json/)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.data.id).toEqual(deliveryId);
+            expect(res.body.data).toEqual(mockUpDelivery);
+          })
+          .end((err, res) => {
+            if (err) return done(err);
+            return done();
+          });
+      });
+  });
+
+  test("POST /deliveries", (done) => {
+    request(kiwiFunctions)
+      .post("/api/v1/users")
+      .expect("Content-Type", /json/)
+      .send(mockUpDelivery)
+      .expect(201)
+      .expect((res) => {
+        expect(res.body.data).toEqual(mockUpDelivery);
       })
       .end((err, res) => {
         if (err) return done(err);
